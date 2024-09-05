d6c09ddc6f17dcb83e9ac7fc607a570e47915245
Prennk <email.affanardana@gmail.com>
2024-09-05 20:28:23 +0700

    remove distribute

--- train_baseline.py
+++ train_baseline.py
@@ -14,9 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
-import torch.distributed as dist
 
-from utils.distributed import *
 from utils.logger import setup_logger
 from utils.score import SegmentationMetric
 
@@ -29,7 +27,6 @@ from dataset.coco_stuff_164k import CocoStuff164kTrainSet, CocoStuff164kValSet
 from utils.flops import cal_multi_adds, cal_param_size
 from models.model_zoo import get_segmentation_model
 from losses import SegCrossEntropyLoss
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Semantic Segmentation Training With Pytorch')
@@ -70,7 +67,6 @@ def parse_args():
     # cuda setting
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
-    parser.add_argument('--local_rank', type=int, default=0)
     # checkpoint and log
     parser.add_argument('--resume', type=str, default=None,
                         help='put the path to resuming file if needed')
@@ -97,24 +93,14 @@ def parse_args():
                         help='run validation every val-epoch')
     parser.add_argument('--skip-val', action='store_true', default=False,
                         help='skip validation during training')
-    parser.add_argument('--distributed', action='store_true', default=False, help='Enable distributed training')
+                        
     args = parser.parse_args()
 
-    num_gpus = int(os.environ.get("WORLD_SIZE", 1))
-    if num_gpus > 1 and args.distributed and args.local_rank == 0:
-        if not os.path.exists(args.log_dir):
-            os.makedirs(args.log_dir)
-        if not os.path.exists(args.save_dir):
-            os.makedirs(args.save_dir)
-
-    # default settings for epochs, batch_size and lr
-
-    if args.backbone.startswith('resnet'):
-        args.aux = True
-    elif args.backbone.startswith('mobile'):
-        args.aux = False
+    if not args.no_cuda and torch.cuda.is_available():
+        cudnn.benchmark = False
+        args.device = "cuda"
     else:
-        raise ValueError('no such network')
+        args.device = "cpu"
 
     return args
     
@@ -124,7 +110,6 @@ class Trainer(object):
     def __init__(self, args):
         self.args = args
         self.device = torch.device(args.device)
-        self.num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
 
         if args.dataset == 'citys':
             train_dataset = CSTrainValSet(args.data, 
@@ -154,30 +139,26 @@ class Trainer(object):
             raise ValueError('dataset unfind')
 
         # create network
-        BatchNorm2d = nn.SyncBatchNorm if args.distributed else nn.BatchNorm2d
+        BatchNorm2d = nn.BatchNorm2d  # Tidak ada distribusi
         self.model = get_segmentation_model(model=args.model,
                                             backbone=args.backbone, 
-                                            local_rank=args.local_rank,
                                             pretrained=args.pretrained, 
                                             pretrained_base=args.pretrained_base,
                                             aux=args.aux, 
                                             norm_layer=BatchNorm2d,
                                             num_class=train_dataset.num_class).to(self.device)
-        
-        if args.distributed:
-            self.model = nn.parallel.DistributedDataParallel(self.model, device_ids=[args.local_rank], output_device=args.local_rank)
 
         self.model.eval()
         with torch.no_grad():
-            logger.info('Params: %.2fM FLOPs: %.2fG'
+            self.logger.info('Params: %.2fM FLOPs: %.2fG'
                 % (cal_param_size(self.model) / 1e6, cal_multi_adds(self.model, (1, 3, 1024, 2048))/1e9))
         
 
-        args.batch_size = args.batch_size // num_gpus
-        train_sampler = make_data_sampler(train_dataset, shuffle=True, distributed=args.distributed)
-        train_batch_sampler = make_batch_data_sampler(train_sampler, args.batch_size, args.max_iterations)
-        val_sampler = make_data_sampler(val_dataset, False, args.distributed)
-        val_batch_sampler = make_batch_data_sampler(val_sampler, 1)
+        args.batch_size = args.batch_size
+        train_sampler = None
+        train_batch_sampler = data.BatchSampler(data.RandomSampler(train_dataset), args.batch_size, drop_last=True)
+        val_sampler = None
+        val_batch_sampler = data.BatchSampler(data.SequentialSampler(val_dataset), 1)
 
         self.train_loader = data.DataLoader(dataset=train_dataset, 
                                             batch_sampler=train_batch_sampler,
@@ -196,196 +177,113 @@ class Trainer(object):
                 assert ext == '.pkl' or '.pth', 'Sorry only .pth and .pkl files supported.'
                 print('Resuming training, loading {}...'.format(args.resume))
                 self.model.load_state_dict(torch.load(args.resume, map_location=lambda storage, loc: storage))
-
-        self.criterion = SegCrossEntropyLoss(ignore_index=args.ignore_label).to(self.device)
-
-        # optimizer, for model just includes pretrained, head and auxlayer
-        params_list = self.model.parameters()
-        self.optimizer = torch.optim.SGD(params_list,
-                                         lr=args.lr,
-                                         momentum=args.momentum,
-                                         weight_decay=args.weight_decay)
-
-        if args.distributed:
-            self.model = nn.parallel.DistributedDataParallel(self.model, device_ids=[args.local_rank],
-                                                             output_device=args.local_rank)
-            
-        # evaluation metrics
-        self.metric = SegmentationMetric(train_dataset.num_class)
-        self.best_pred = 0.0
-
-    def adjust_lr(self, base_lr, iter, max_iter, power):
-        cur_lr = base_lr*((1-float(iter)/max_iter)**(power))
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] = cur_lr
-
-        return cur_lr
-
-    def reduce_tensor(self, tensor):
-        if self.args.distributed:
-            rt = tensor.clone()
-            dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-            return rt
-        return tensor
-
-
-    def reduce_tensor(self, tensor):
-        if self.args.distributed:
-            rt = tensor.clone()
-            dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-            return rt
-        return tensor
-
-        
-    def train(self):
-        save_to_disk = get_rank() == 0 if self.args.distributed else True
-        log_per_iters, val_per_iters = self.args.log_iter, self.args.val_per_iters
-        save_per_iters = self.args.save_per_iters
-        start_time = time.time()
-        logger.info('Start training, Total Iterations {:d}'.format(args.max_iterations))
-
-        self.model.train()
-        
-        for iteration,  (images, targets, _) in enumerate(self.train_loader):
-            iteration = iteration + 1
-            images = images.to(self.device)
-            targets = targets.long().to(self.device)
-
-            outputs = self.model(images)
-            
-            if self.args.aux:
-                task_loss = self.criterion(outputs[0], targets) + 0.4 * self.criterion(outputs[1], targets)
             else:
-                task_loss = self.criterion(outputs[0], targets)
+                raise ValueError('Checkpoint file not found')
 
-            losses = task_loss
-            
-
-            lr = self.adjust_lr(base_lr=args.lr, iter=iteration-1, max_iter=args.max_iterations, power=0.9)
-            self.optimizer.zero_grad()
-            losses.backward()
-            self.optimizer.step()
-
-            # reduce losses over all GPUs for logging purposes
-            task_losses_reduced = self.reduce_mean_tensor(task_loss)
-            
-            eta_seconds = ((time.time() - start_time) / iteration) * (args.max_iterations - iteration)
-            eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
-
-            if iteration % log_per_iters == 0 and save_to_disk:
-                logger.info(
-                    "Iters: {:d}/{:d} || Lr: {:.6f} || Task Loss: {:.4f} || Cost Time: {} || Estimated Time: {}".format(
-                        iteration, args.max_iterations, self.optimizer.param_groups[0]['lr'], task_losses_reduced.item(),
-                        str(datetime.timedelta(seconds=int(time.time() - start_time))), eta_string))
-
-            if iteration % save_per_iters == 0 and save_to_disk:
-                save_checkpoint(self.model, self.args, is_best=False)
-
-            if not self.args.skip_val and iteration % val_per_iters == 0:
-                self.validation()
-                self.model.train()
-
-        save_checkpoint(self.model, self.args, is_best=False)
-        total_training_time = time.time() - start_time
-        total_training_str = str(datetime.timedelta(seconds=total_training_time))
-        logger.info(
-            "Total training time: {} ({:.4f}s / it)".format(
-                total_training_str, total_training_time / args.max_iterations))
-
-
-    def validation(self):
-        # total_inter, total_union, total_correct, total_label = 0, 0, 0, 0
-        is_best = False
-        self.metric.reset()
-        if self.args.distributed:
-            model = self.model.module
+        # create optimizer
+        if args.optimizer == 'adam':
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        elif args.optimizer == 'sgd':
+            self.optimizer = torch.optim.SGD(self.model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
         else:
-            model = self.model
-        torch.cuda.empty_cache()  # TODO check if it helps
+            raise ValueError('Optimizer not supported')
+
+        # create loss function
+        self.criterion = SegCrossEntropyLoss(ignore_index=args.ignore_label, aux_weight=args.aux_weight).to(self.device)
+        
+        # create logger
+        self.logger = setup_logger('train', args.log_dir, 0)
+        self.logger.info('Training with args: %s', args)
+        
+        # initialize training states
+        self.best_miou = 0.0
+
+    def adjust_lr(self, optimizer, iteration, args):
+        lr = args.lr * (0.1 ** (iteration // (args.max_iterations // 3)))
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def train(self):
+        args = self.args
+        model = self.model
+        criterion = self.criterion
+        optimizer = self.optimizer
+        train_loader = self.train_loader
+        val_loader = self.val_loader
+        logger = self.logger
+
+        # Set the model to training mode
+        model.train()
+
+        start_time = time.time()
+        for iteration, (images, labels) in enumerate(train_loader):
+            if iteration >= args.max_iterations:
+                break
+            if (iteration+1) % args.val_per_iters == 0:
+                self.validation(iteration)
+
+            # Move data to device
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Adjust learning rate
+            if (iteration+1) % args.log_iter == 0:
+                self.adjust_lr(optimizer, iteration, args)
+
+            if (iteration+1) % args.log_iter == 0:
+                logger.info('Iteration [%d/%d], Loss: %.4f, Time: %.2f'
+                             % (iteration + 1, args.max_iterations, loss.item(), time.time() - start_time))
+
+            if (iteration+1) % args.save_per_iters == 0:
+                self.save_checkpoint(iteration)
+
+    def validation(self, iteration):
+        model = self.model
+        val_loader = self.val_loader
+        logger = self.logger
+
         model.eval()
-        logger.info("Start validation, Total sample: {:d}".format(len(self.val_loader)))
+        metric = SegmentationMetric(num_classes=val_loader.dataset.num_class)
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+
+                outputs = model(images)
+                preds = torch.argmax(outputs, dim=1)
+                metric.update(labels.cpu().numpy(), preds.cpu().numpy())
+
+        pixel_acc, mIoU = metric.get()
+        logger.info('Validation Result - Iteration [%d], Pixel Accuracy: %.4f, mIoU: %.4f'
+                     % (iteration + 1, pixel_acc, mIoU))
         
-        for i, (image, target, filename)  in enumerate(self.val_loader):
-            image = image.to(self.device)
-            target = target.long().to(self.device)
+        # Save the best model
+        if mIoU > self.best_miou:
+            self.best_miou = mIoU
+            self.save_checkpoint(iteration, is_best=True)
 
-            with torch.no_grad():
-                outputs = model(image)
-            
-            B, H, W = target.size()
-            outputs[0] = F.interpolate(outputs[0], (H, W), mode='bilinear', align_corners=True)
-
-            self.metric.update(outputs[0], target)
-            pixAcc, mIoU = self.metric.get()
-            logger.info("Sample: {:d}, Validation pixAcc: {:.3f}, mIoU: {:.3f}".format(i + 1, pixAcc, mIoU))
-        
-        if self.num_gpus > 1:
-            sum_total_correct = torch.tensor(self.metric.total_correct).cuda().to(args.local_rank)
-            sum_total_label = torch.tensor(self.metric.total_label).cuda().to(args.local_rank)
-            sum_total_inter = torch.tensor(self.metric.total_inter).cuda().to(args.local_rank)
-            sum_total_union = torch.tensor(self.metric.total_union).cuda().to(args.local_rank)
-            sum_total_correct = self.reduce_tensor(sum_total_correct)
-            sum_total_label = self.reduce_tensor(sum_total_label)
-            sum_total_inter = self.reduce_tensor(sum_total_inter)
-            sum_total_union = self.reduce_tensor(sum_total_union)
-
-            pixAcc = 1.0 * sum_total_correct / (2.220446049250313e-16 + sum_total_label)  # remove np.spacing(1)
-            IoU = 1.0 * sum_total_inter / (2.220446049250313e-16 + sum_total_union)
-            mIoU = IoU.mean().item()
-
-            logger.info("Overall validation pixAcc: {:.3f}, mIoU: {:.3f}".format(
-            pixAcc.item() * 100, mIoU * 100))
-
-        new_pred = mIoU
-        if new_pred > self.best_pred:
-            is_best = True
-            self.best_pred = new_pred
-        if args.local_rank == 0:
-            save_checkpoint(self.model, self.args, is_best)
-        synchronize()
-
-
-def save_checkpoint(model, args, is_best=False):
-    """Save Checkpoint"""
-    directory = os.path.expanduser(args.save_dir)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    filename = '{}_{}_{}.pth'.format(args.model, args.backbone, args.dataset)
-    filename = os.path.join(directory, filename)
-
-    if args.distributed:
-        model = model.module
-    
-    torch.save(model.state_dict(), filename)
-    if is_best:
-        best_filename = '{}_{}_{}_best_model.pth'.format(args.model, args.backbone, args.dataset)
-        best_filename = os.path.join(directory, best_filename)
-        shutil.copyfile(filename, best_filename)
-
+    def save_checkpoint(self, iteration, is_best=False):
+        state = {
+            'iteration': iteration,
+            'state_dict': self.model.state_dict(),
+            'best_miou': self.best_miou,
+        }
+        filename = os.path.join(self.args.save_dir, 'checkpoint.pth')
+        torch.save(state, filename)
+        if is_best:
+            shutil.copyfile(filename, os.path.join(self.args.save_dir, 'model_best.pth'))
+        self.logger.info('Checkpoint saved to %s', filename)
 
 if __name__ == '__main__':
     args = parse_args()
-
-    # reference maskrcnn-benchmark
-    num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
-    args.num_gpus = num_gpus
-    args.distributed = num_gpus > 1
-    if not args.no_cuda and torch.cuda.is_available():
-        cudnn.benchmark = False
-        args.device = "cuda"
-    else:
-        args.distributed = False
-        args.device = "cpu"
-    if args.distributed:
-        torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(backend="nccl", init_method="env://")
-        synchronize()
-
-    logger = setup_logger("semantic_segmentation", args.log_dir, get_rank(), filename='{}_{}_{}_log.txt'.format(
-        args.model, args.backbone, args.dataset))
-    logger.info("Using {} GPUs".format(num_gpus))
-    logger.info(args)
-
     trainer = Trainer(args)
     trainer.train()
-    torch.cuda.empty_cache()
