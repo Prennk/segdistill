82db8e1571f7f785610f403bc4a581d73392e176
Prennk <email.affanardana@gmail.com>
2024-09-05 18:36:18 +0700

    add train sh

--- train_scripts/train_baseline/camvid/deeplabv3_res18.sh
+++ train_scripts/train_baseline/camvid/deeplabv3_res18.sh
@@ -1,21 +1,21 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-    python -m torch.distributed.launch --nproc_per_node=8 train_baseline.py \
+    python train_baseline.py \
     --model deeplabv3 \
     --backbone resnet18 \
     --dataset camvid \
-    --crop-size 360 360 \
-    --data [your dataset path]/CamVid/ \
-    --save-dir [your directory path to store checkpoint files] \
-    --log-dir [your directory path to store log files] \
-    --pretrained-base [your pretrained-backbone path]/resnet18-imagenet.pth
+    --crop-size 480 360 \
+    --data data/CamVid/ \
+    --save-dir save/ \
+    --log-dir save/ \
 
 
-CUDA_VISIBLE_DEVICES=0 \
-  python -m torch.distributed.launch --nproc_per_node=1 \
-  eval.py \
-  --model deeplabv3 \
-  --backbone resnet18 \
-  --dataset camvid \
-  --data [your dataset path]/CamVid/ \
-  --save-dir [your directory path to store checkpoint files] \
-  --pretrained [your pretrained model path]
+# CUDA_VISIBLE_DEVICES=0 \
+#   python -m torch.distributed.launch --nproc_per_node=1 \
+#   eval.py \
+#   --model deeplabv3 \
+#   --backbone resnet18 \
+#   --dataset camvid \
+#   --data [your dataset path]/CamVid/ \
+#   --save-dir [your directory path to store checkpoint files] \
+#   --pretrained [your pretrained model path]
+
+# python3 eval.py --model deeplabv3 --backbone resnet101 --dataset camvid --data ../../semantic-seg-distillation/data/CamVid/ --data-list ../../semantic-seg-distillation/data/CamVid/test.txt --crop-size 480 360 --pretrained ../../semantic-seg-distillation/deeplabv3_resnet101_cirkd.pth
