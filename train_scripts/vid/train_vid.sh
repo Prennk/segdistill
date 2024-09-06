2e9a31a01a7dfb63e7b75eaf480ac93fc2ec7885
Prennk <email.affanardana@gmail.com>
2024-09-06 16:34:43 +0700

    add script

---
+++ train_scripts/vid/train_vid.sh
@@ -0,0 +1,23 @@
+python3 train_vid.py \
+    --teacher-model deeplabv3 \
+    --student-model deeplabv3 \
+    --teacher-backbone resnet101 \
+    --student-backbone resnet18 \
+    --max-iterations 15000 \
+    --crop-size 480 360 \
+    --dataset camvid \
+    --data data/CamVid/ \
+    --save-dir save/ \
+    --log-dir save/ \
+    --teacher-pretrained deeplabv3_resnet101_cirkd.pth \
+    --pretrained-base resnet18-imagenet.pth
+
+
+# python3 eval.py \
+#     --model deeplabv3 \
+#     --backbone resnet18 \
+#     --dataset camvid \
+#     --data data/CamVid/ \
+#     --data-list data/CamVid/test.txt \
+#     --crop-size 480 360 \
+#     --pretrained save/deeplabv3_resnet18_camvid_best_model.pth \
